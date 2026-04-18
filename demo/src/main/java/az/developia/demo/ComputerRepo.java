d8b6eefee4363acbe341fac00e5b8a1e22892540
sabir kral <sabirmemmedli21152014@gmail.com>
2026-04-18 17:26:31 +0400

    s

--- demo/src/main/java/az/developia/demo/ComputerRepo.java
+++
@@ -1,9 +0,0 @@
-package az.developia.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ComputerRepo extends JpaRepository<ComputerEntity ,Long> {
-
-}
--- demo/src/main/java/az/developia/demo/DemoApplication.java
+++ demo/src/main/java/az/developia/demo/DemoApplication.java
@@ -2,12 +2,12 @@ package az.developia.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class DemoApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
 }
